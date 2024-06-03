15c8db94e5c29eaec872f4025e9fc68af1f0d45c
Anemieke <anemieke.van.vuuren16@gmail.com>
2024-06-03 16:10:09 +0200

    updating list groups

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -21,14 +21,10 @@ function ListGroup() {
     items = [];
 
 
-    const getMessage = () => {
-        return items.length === 0 ? <p>No items found</p> : null;
-    }
-
   return (   
     <>
         <h1>List </h1>
-        {getMessage()} 
+        {items.length === 0 && <p>No items found</p>}
         <ul className="list-group">
             {items.map((item) => (
                 <li key={item}>{item}</li>
