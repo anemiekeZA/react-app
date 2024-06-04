353d278153db2cdf4b16bd434f71558b59459d44
Anemieke <anemieke.van.vuuren16@gmail.com>
2024-06-04 13:20:46 +0200

    Adding in button string literals, (It doesn't need to remain as one colour we can give it options)

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,7 @@ import React  from "react";
 
 interface Props {
     children: string;
-    color?: string;
+    color?: 'primary'| 'secondary'| 'danger'; // string literal
     onClick: () => void;
 }
 
