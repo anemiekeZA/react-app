211d537d66dbd6247bdb22f6cc98cbd575dfaf96
Anemieke <anemieke.van.vuuren16@gmail.com>
2024-07-05 08:28:47 +0200

    adding in icons

--- src/components/LeftPannel.tsx
+++ src/components/LeftPannel.tsx
@@ -2,6 +2,16 @@ import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import "../Styles/main.css";
 import { Link } from 'react-router-dom';
+import { CgGames } from "react-icons/cg";
+import { MdGames } from "react-icons/md";
+import { FaLaptopCode } from "react-icons/fa";
+import { GiChampions } from "react-icons/gi";
+import { GiPodiumWinner } from "react-icons/gi";
+import { FaPlaystation } from "react-icons/fa";
+import { IoLogoAndroid } from "react-icons/io";
+import { GiBackwardTime } from "react-icons/gi";
+import { IoLogoXbox } from "react-icons/io5";
+
 
 function LeftPannel() {
   const navRef = useRef();
@@ -10,15 +20,15 @@ function LeftPannel() {
     <nav ref={navRef} >
       <div>
         <ul className="left-side-nav"> 
-          <li><Link to="/latest-games">Latest Games</Link></li>
-          <li><Link to="/best-of-the-year">Best of the Year</Link></li>
-          <li><Link to="/popular">Popular in </Link></li>
-          <li><Link to="/contact-us">Last Year</Link></li>
-          <li><Link to="/all-time-top-250">All Time Top 250</Link></li>
-          <li><Link to="/pc-games">PC Games</Link></li>
-          <li><Link to="/xbox-games">Xbox Games</Link></li>
-          <li><Link to="/ps-games">PS Games</Link></li>
-          <li><Link to="/android-games">Android Games</Link></li>
+          <li><Link to="/latest-games"><CgGames /> Latest Games</Link></li>
+          <li><Link to="/best-of-the-year"><GiPodiumWinner /> Best of the Year</Link></li>
+          <li><Link to="/popular"><MdGames /> Popular in </Link></li>
+          <li><Link to="/contact-us"><GiBackwardTime /> Last Year</Link></li>
+          <li><Link to="/all-time-top-250"><GiChampions /> All Time Top 250</Link></li>
+          <li><Link to="/pc-games"><FaLaptopCode /> PC Games</Link></li>
+          <li><Link to="/xbox-games"><IoLogoXbox /> Xbox Games</Link></li>
+          <li><Link to="/ps-games"><FaPlaystation /> PS Games</Link></li>
+          <li><Link to="/android-games"><IoLogoAndroid /> Android Games</Link></li>
         </ul>
       </div>
     </nav>
